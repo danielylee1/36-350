6e9262152a85640c501f189cd0dda91fcc009bba
Daniel Y. Lee <danielylee@leed.wv.cc.cmu.edu>
2018-04-19 17:23:29 -0400

    2b

--- Lab_12_simulation.R
+++ Lab_12_simulation.R
@@ -1,5 +1,21 @@
-generate_data = function(n, p){
-  covariate = matrix(data = rnorm(n*p, mean=0, sd=1), nrows = n, ncols = p)
-  response = rnorm(n, 0, 1)
-  return(list(covariate = covariate, response = response))
+generate_data = function(n,p) 
+{
+  covariates = matrix(rnorm(n*p, 0, 1), nrow = n, ncol = p)
+  responses = rnorm(n, 0, 1)
+  return(list(covariates = covariates, responses = responses))
 }
+
+model_select = function(covariates, responses, cutoff) 
+{
+  regress = lm(responses ~ covariates)
+  pval = (summary(regress)$coefficients)[-1, 4]
+  withinCut = which(pval <= cutoff)
+  if (length(withinCut) == 0) 
+  {
+    return(c())
+  }
+  regress = lm(responses ~ covariates[, withinCut])
+  pval2 = (summary(resgress)$coefficients)[-1, 4]
+  return(pval2)
+}
+
